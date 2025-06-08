b71419b8e144767fb678c80b674d1c5d14e639df
unknown <viperdex2000@hotmail.com>
2025-06-07 22:28:55 -0500

    Refactorizacion del componente WheatherInfo

--- src/components/WheatherInfo.jsx
+++ src/components/WheatherInfo.jsx
@@ -10,45 +10,47 @@ function WeatherInfo() {
 
     // Context Called:
     const AppContext = useAppcontext();
+    const { error, temperature, city, country, cloudy, description, humidity, wind_speed } = AppContext.value;
+
+    if (error) {
+        return (
+            <div className="w-full h-full">
+                <p>{error}</p>
+            </div>
+        );
+    }
 
     return (
 
         <div className="w-full h-full">
-
-            {
-                AppContext.value.error &&
-                <div>
-                    <p>{AppContext.value.error}</p>
-                </div>
-            }
-            {AppContext.value.temperature ?
+            {temperature ?
                 <div className="w-full h-full text-lg sm:text-xl">
 
                     <h1 className="my-4 sm:mt-8">Weather Details</h1>
 
                     <div className="hidden sm:w-full sm:flex sm:justify-between sm:my-8">
                         <p className="text-gray-500 font-medium">Location</p>
-                        <p>{AppContext.value.city}, {AppContext.value.country}</p>
+                        <p>{city}, {country}</p>
                     </div>
 
                     <div className="w-full flex justify-between mb-2 sm:mb-8">
                         <p className="text-gray-500 font-medium">Cloudy</p>
-                        <p>{AppContext.value.cloudy}</p>
+                        <p>{cloudy}</p>
                     </div>
 
                     <div className="w-full flex justify-between mb-2 sm:mb-8">
                         <p className="text-gray-500 font-medium">Description</p>
-                        <p>{AppContext.value.description}</p>
+                        <p>{description}</p>
                     </div>
 
                     <div className="w-full flex justify-between mb-2 sm:mb-8">
                         <p className="text-gray-500 font-medium">Humidity</p>
-                        <p>{AppContext.value.humidity}</p>
+                        <p>{humidity}</p>
                     </div>
 
                     <div className="w-full flex justify-between">
                         <p className="text-gray-500 font-medium">Wind Speed</p>
-                        <p>{AppContext.value.wind_speed}</p>
+                        <p>{wind_speed}</p>
                     </div>
 
                 </div>
